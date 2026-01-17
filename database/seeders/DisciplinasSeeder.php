35a16b55ef754e455cb3687b7d664969e83ffd03
GiovaniSusin <giovani.susin@gmail.com>
2026-01-16 23:29:09 -0300

    seeder para preencher banco de dados

---
+++ database/seeders/DisciplinasSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Disciplina;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\File;
+
+
+class DisciplinasSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $json = File::get('database/data/dataset.json');
+        $data = json_decode($json);
+
+        foreach ($data as $disciplina)
+            {
+                Disciplina::create(array(
+                    'codigo' => $disciplina->codigo,
+                    'nome' => $disciplina->nome,
+                    'etapa' => $disciplina->etapa,
+                    'carater' => $disciplina->carater,
+                    'responsavel' => $disciplina->responsavel,
+                    'creditos' => $disciplina->creditos,
+                    'descricao' => $disciplina->descricao,
+                    'ead' => $disciplina->ead,
+                    'extensionista' => $disciplina->extensionista,
+                    'extracurricular' => $disciplina->extracurricular
+                ));
+            }
+    }
+}
